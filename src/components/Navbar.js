f76ee6a227312cc44733b49260cd226fe0f3e5dc
Joshua <fleshmanjoshuac@hotmail.com>
2023-05-11 20:35:04 -0500

    added 00-20-1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,10 @@ function Navbar(props) {
         props.onFileChange(file);
     }
 
+    function openWebsite() {
+  window.open('https://github.com/fleshmanj/react/blob/main/src/pdfs/33%20MXG%2000-20-1.pdf', '_blank');
+}
+
     return (
         <nav>
             <ul>
@@ -12,7 +16,7 @@ function Navbar(props) {
                 <li><button onClick={() => handleClick('https://static.e-publishing.af.mil/production/1/aetc/publication/afi21-101_aetcsup/afi21-101_aetcsup.pdf')}>AFI21-101 AETC Sup</button></li>
                 <li><button onClick={() => handleClick('https://static.e-publishing.af.mil/production/1/aetc/publication/afi21-101_aetcsup_add_a/afi21-101_aetcsup_add_a.pdf')}>AFI 21-101 AETC Sup Addendum A</button></li>
                 <li><button onClick={() => handleClick('https://static.e-publishing.af.mil/production/1/33fw/publication/afi21-101_aetcsup_33fwsup/afi21-101_aetcsup_33fw_sup.pdf')}>AFI 21-101 33FW Sup</button></li>
-                <li><button onClick={() => handleClick('src/pdfs/33 MXG 00-20-1.pdf')}>TO 00-20-1</button></li>
+                <li><button onClick={() => handleClick('https://fleshmanj.github.io/filestore/PDF/33%20MXG%2000-20-1.pdf')}>TO 00-20-1 33FW Sup</button></li>
             </ul>
         </nav>
     );
--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,40 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function PdfViewer(props) {
-    const [filePath, setFilePath] = useState(props.file);
-    const [error, setError] = useState(null);
+  const [pdfUrl, setPdfUrl] = useState(null);
 
-    function onDocumentLoadError(error) {
-        try {
-            // Silently ignore the error
-        } catch (error) {
-            console.error(error);
+  useEffect(() => {
+    const fetchPdf = async () => {
+      try {
+        if (props.file.includes('github')) {
+          const response = await fetch(props.file);
+          const arrayBuffer = await response.arrayBuffer();
+          const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
+          const url = URL.createObjectURL(blob);
+          setPdfUrl(url);
+        } else {
+          setPdfUrl(props.file);
         }
-    }
+      } catch (error) {
+        console.error('Error fetching PDF:', error);
+      }
+    };
 
-    function onIframeLoad() {
-        try {
-            const iframe = document.getElementById('pdfViewer');
-            iframe.style.height = `${window.innerHeight}px`;
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    fetchPdf();
+  }, [props.file]);
 
-    useEffect(() => {
-        setFilePath(props.file);
-        setError(null);
-    }, [props.file]);
-
-    if (error) {
-        return <div>Failed to load PDF: {error.message}</div>;
-    }
-
-    return (
-        <div style={{ height: '1200px' }}>
-            <iframe id="pdfViewer" src={filePath} onLoad={onIframeLoad} style={{ width: '100%', height: "1200px", border: 'none' }}></iframe>
-        </div>
-    );
+  return (
+    <div>
+      <iframe src={pdfUrl} title="PDF Viewer" width="100%" height="600px" />
+    </div>
+  );
 }
 
 export default PdfViewer;
